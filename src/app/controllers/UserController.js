f8f80f784b99cdbe13620210a225b9dd997a3427
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-07-18 01:59:37 -0300

    feat: authorization middleware and update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,9 +10,31 @@ class UserController {
       return res.status(400).json({ error: 'User already exists' })
     }
 
-    const { id, name, email, providers } = await User.create(req.body)
+    const { id, name, email, provider } = await User.create(req.body)
 
-    return res.json({ id, name, email, providers })
+    return res.json({ id, name, email, provider })
+  }
+
+  async update(req, res) {
+    const { email, password_old } = req.body
+
+    const user = await User.findByPk(req.userId)
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email: req.body.email }
+      })
+
+      if (userExists) {
+        return res.status(400).json({ error: 'E-mail already exists' })
+      }
+    }
+
+    if (password_old && !(await user.checkPassword(password_old))) {
+      return res.status(401).json({ error: 'Incorrect password' })
+    }
+    const { id, name, provider } = await user.update(req.body)
+    return res.json({ id, name, email, provider })
   }
 }
 
