f268ed9d7d7835e9281ced4b4a744011008a7c12
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-07-20 01:33:31 -0300

    add: image upload with multer (local disk)

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import authMiddleware from './app/middlewares/auth'
+import multerConfig from './config/multer'
 
 const routes = new Router()
+const upload = multer(multerConfig)
 
 routes.post('/auth', SessionController.store)
 
@@ -13,5 +16,9 @@ routes.post('/users', UserController.store)
 routes.use(authMiddleware)
 
 routes.put('/users', UserController.update)
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ saveImage: true })
+})
 
 export default routes
