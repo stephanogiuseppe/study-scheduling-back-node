e15b5b06ee5d502c409fb1c24cd49ac84f03a716
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-07-27 01:53:25 -0300

    fix: remove request from beforeEach in user.spec

--- __tests__/integration/user.spec.js
+++ __tests__/integration/user.spec.js
@@ -4,12 +4,7 @@ import app from '../../src/app'
 import truncate from '../utils/truncate'
 
 describe('User', () => {
-  beforeEach(async () => {
-    truncate()
-    await request(app)
-      .post('/users')
-      .send({ name: 'Test', email: 'test@mail.com', password: 'password1' })
-  })
+  beforeEach(async () => truncate())
 
   describe('store function', () => {
     it('Should be register an user', async () => {
@@ -99,11 +94,15 @@ describe('User', () => {
     it('Should not update an user with email duplicated', async () => {
       await request(app)
         .post('/users')
+        .send({ name: 'Test', email: 'test@mail.com', password: 'password1' })
+
+      await request(app)
+        .post('/users')
         .send({ name: 'Ste', email: 'ste@email.com', password: 'weakPassword' })
 
       const resCredentials = await request(app)
         .post('/auth')
-        .send({ email: 'test@mail.com', password: 'password1' })
+        .send({ email: 'ste@email.com', password: 'weakPassword' })
 
       const credentials = JSON.parse(resCredentials.text)
 
@@ -112,9 +111,9 @@ describe('User', () => {
         .set('Authorization', `Bearer ${credentials.token}`)
         .send({
           name: 'Ste',
-          email: 'ste@email.com',
+          email: 'test@mail.com',
           password: 'strongPassword',
-          password_old: 'password1',
+          password_old: 'weakPassword',
           password_confirmation: 'strongPassword'
         })
 
