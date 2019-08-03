bed13b0d480543e90aca4c9bdbc07482fed9dee3
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-08-03 03:13:38 -0300

    test: add appointment

---
+++ __tests__/integration/appointment.spec.js
@@ -0,0 +1,72 @@
+import request from 'supertest'
+
+import app from '../../src/app'
+import truncate from '../utils/truncate'
+
+describe('Appointment', () => {
+  beforeEach(async () => truncate())
+
+  describe('Store functions', () => {
+    it('Should be register an appointment', async () => {
+      const provider = await request(app)
+        .post('/users')
+        .send({
+          name: 'Provider',
+          email: 'provider@email.com',
+          password: 'password1',
+          provider: true
+        })
+
+      await request(app)
+        .post('/users')
+        .send({ name: 'Ste', email: 'ste@email.com', password: 'weakPassword' })
+
+      const resCredentials = await request(app)
+        .post('/auth')
+        .send({ email: 'ste@email.com', password: 'weakPassword' })
+
+      const credentials = JSON.parse(resCredentials.text)
+
+      const res = await request(app)
+        .post('/appointments')
+        .set('Authorization', `Bearer ${credentials.token}`)
+        .send({
+          provider_id: provider.body.id,
+          date: '2019-09-01T18:00:00-03:00'
+        })
+
+      expect(res.body).toHaveProperty('date')
+    })
+
+    it('Should not be register an appointment with past date', async () => {
+      await request(app)
+        .post('/users')
+        .send({ name: 'Ste', email: 'ste@email.com', password: 'weakPassword' })
+
+      const resCredentials = await request(app)
+        .post('/auth')
+        .send({ email: 'ste@email.com', password: 'weakPassword' })
+
+      const provider = await request(app)
+        .post('/users')
+        .send({
+          name: 'Provider',
+          email: 'provider@email.com',
+          password: 'password1',
+          provider: true
+        })
+
+      const credentials = JSON.parse(resCredentials.text)
+
+      const res = await request(app)
+        .post('/appointments')
+        .set('Authorization', `Bearer ${credentials.token}`)
+        .send({
+          provider_id: provider.body.id,
+          date: '2019-06-01T18:00:00-03:00'
+        })
+
+      expect(res.status).toBe(400)
+    })
+  })
+})
