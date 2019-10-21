59faecb3a8a6375e9a00c30ae1024b24e647f439
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-10-21 00:57:33 -0300

    feat(exceptions): errors handle online

--- src/app.js
+++ src/app.js
@@ -1,19 +1,27 @@
-import './bootstrap'
-
 import path from 'path'
 import express from 'express'
+import * as Sentry from '@sentry/node'
+import 'express-async-errors'
+import Youch from 'youch'
 
+import './bootstrap'
 import './database'
 import routes from './routes'
+import sentryConfig from './config/sentry'
 
 class App {
   constructor() {
     this.server = express()
+
+    Sentry.init(sentryConfig)
+
     this.middlewares()
     this.routes()
+    this.exceptionHandler()
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler())
     this.server.use(express.json())
     this.server.use(
       '/files',
@@ -23,6 +31,14 @@ class App {
 
   routes() {
     this.server.use(routes)
+    this.server.use(Sentry.Handlers.errorHandler())
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON()
+      res.status(500).json(errors)
+    })
   }
 }
 
