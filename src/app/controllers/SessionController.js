b623a76c8a02b6a76bd8b88f54d0c8ee6604ae5f
stephanogiuseppe <stephano.grp@gmail.com>
2020-03-17 18:33:51 -0300

    feat(SessionControler):  get user with avatar data

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup'
 import jwt from 'jsonwebtoken'
 
 import User from '../models/User'
+import File from '../models/File'
 import authConfig from '../../config/auth'
 
 class SessionController {
@@ -21,7 +22,16 @@ class SessionController {
     const { email, password } = req.body
 
     try {
-      user = await User.findOne({ where: { email } })
+      user = await User.findOne({
+        where: { email },
+        include: [
+          {
+            model: File,
+            as: 'avatar',
+            attributes: ['id', 'path', 'url']
+          }
+        ]
+      })
     } catch (error) {
       res.status(500).json({ error: 'Web server is down' })
     }
@@ -34,10 +44,10 @@ class SessionController {
       return res.status(401).json({ error: 'Incorrect password' })
     }
 
-    const { id, name } = user
+    const { id, name, avatar } = user
 
     return res.json({
-      user: { id, name, email },
+      user: { id, name, email, avatar },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn
       })
